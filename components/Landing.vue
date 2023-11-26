a78446692e7174e8c1205502aaa2e88eb926e942
darko <darkonikoloc@gmail.com>
2023-11-26 15:22:05 +0100

    new contact on landing view

--- components/Landing.vue
+++ components/Landing.vue
@@ -23,6 +23,11 @@
 
 <template>
   <section id="landing" class="relative flex flex-col justify-center w-full h-[100dvh] md:pl-10 xm:pl-20">
+    <div class="absolute top-3 right-3 flex justify-center gap-5">
+      <a href="https://github.com/dqrk0jeste" target="_blank"><button class="p-2 md:p-3 text-black border-solid border-black border-2 rounded-2xl flex items-center justify-center  hover:bg-white/10"><i class='bx bxl-github bx-md'></i></button></a>
+      <a href="https://www.linkedin.com/in/darko-nikolic-99816b296/" target="_blank"><button class="p-2 md:p-3 text-black border-solid border-black border-2 rounded-2xl flex items-center justify-center  hover:bg-white/10"><i class='bx bxl-linkedin bx-md'></i></button></a>
+      <a href="mailto:darkonikoloc@gmail.com" target="_blank"><button class="p-2 md:p-3 text-black border-solid border-black border-2 rounded-2xl flex items-center justify-center hover:bg-white/10"><i class='bx bx-envelope bx-md'></i></button></a>
+    </div>
     <div class="text-center w-full border-solid border-black md:pl-10 xm:pl-20 md:text-left md:border-l-4">
       <p class="title font-bold whitespace-nowrap text-3xl sm:text-4xl md:text-5xl xm:text-6xl lg:text-8xl xl:text-9xl">DARKO NIKOLIC</p>
       <p class="title font-bold whitespace-nowrap text-2xl sm:text-3xl md:text-4xl xm:text-5xl lg:text-7xl xl:text-8xl">{{ landingText }}<span v-if="landingText.length !== wholeLandingText.length">_</span></p>
