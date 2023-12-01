1086a80e756a3fd2543b95317f142303b42955ac
darko <darkonikoloc@gmail.com>
2023-12-01 07:40:34 +0100

    refactored skills once more

--- components/MySkills.vue
+++ components/MySkills.vue
@@ -8,6 +8,9 @@
     })
   }
 
+  const selectedSkill: Ref<number> = useState('selected-skill', () => 1)
+  const direction: Ref<number> = useState('direction', () => -2)
+
   const skills = {
     backend: [
       {
@@ -64,15 +67,26 @@
       },
     ]
   }
+
+  watch(selectedSkill, (to, from) => {
+    direction.value = to - from
+  })
 </script>
 
 <template>
-  <div class="custom-gradient w-100% h-[100dvh] flex flex-col px-5 justify-center">
+  <div class="custom-gradient w-100% h-[100dvh]">
     <h2 class="text-center text-6xl font-bold text-white py-5 md:py-10">MY STACK</h2>
+    <div class="w-full flex justify-center items-center text-white text-3xl">
+      <h3 class="px-5 border-solid border-pink-400 transition-[padding] duration-300 cursor-pointer" :class="selectedSkill === 1 ? 'border-b-2 pb-3' : ''" @click="selectedSkill = 1">Backend</h3>
+      <h3 class="px-5 border-solid border-pink-400 transition-[padding] duration-300 cursor-pointer" :class="selectedSkill === 2 ? 'border-b-2 pb-3' : ''" @click="selectedSkill = 2">Databases</h3>
+      <h3 class="px-5 border-solid border-pink-400 transition-[padding] duration-300 cursor-pointer" :class="selectedSkill === 3 ? 'border-b-2 pb-3' : ''" @click="selectedSkill = 3">Frontend</h3>
+    </div>
     <div>
-      <SkillSet groupName="BACKEND" :entries="skills.backend"/>
-      <SkillSet groupName="DATABASES" :entries="skills.database"/>
-      <SkillSet groupName="FRONTEND" :entries="skills.frontend"/>
+      <Transition :name="direction < 0 ? 'appearFromRight' : 'appearFromLeft'" mode="out-in">
+        <SkillSet key="backend" v-if="selectedSkill === 1" :entries="skills.backend"/>
+        <SkillSet key="databases" v-else-if="selectedSkill === 2" :entries="skills.database"/>
+        <SkillSet key="frontend" v-else :entries="skills.frontend"/>
+      </Transition>
     </div>
   </div>
 </template>
@@ -80,5 +94,56 @@
 <style scoped>
   .custom-gradient {
     background: linear-gradient(179.4deg, rgb(12, 20, 69) -16.9%, rgb(71, 30, 84) 119.9%);
+  }
+
+  .appearFromRight-enter-active,
+  .appearFromRight-leave-active {
+    transition: all 0.3s;
+}
+
+  .appearFromRight-enter-from {
+    opacity: 0;
+    transform: translate(-50%);
+  }
+
+  .appearFromRight-enter-to {
+    opacity: 1;
+    transform: translate(0);
+  }
+
+  .appearFromRight-leave-from {
+      opacity: 1;
+      transform: translate(0);
+  }
+
+  .appearFromRight-leave-to {
+      opacity: 0;
+      transform: translate(50%);
+  }
+
+  
+  .appearFromLeft-enter-active,
+  .appearFromLeft-leave-active {
+    transition: all 0.3s;
+}
+
+  .appearFromLeft-enter-from {
+    opacity: 0;
+    transform: translate(50%);
+  }
+
+  .appearFromLeft-enter-to {
+    opacity: 1;
+    transform: translate(0);
+  }
+
+  .appearFromLeft-leave-from {
+      opacity: 1;
+      transform: translate(0);
+  }
+
+  .appearFromLeft-leave-to {
+      opacity: 0;
+      transform: translate(-50%);
   }
 </style>
--- components/SkillSet.vue
+++ components/SkillSet.vue
@@ -6,12 +6,13 @@
 </script>
 
 <template>
-  <div class="max-w-[800px] mx-auto p-5 my-5 border-solid border-white border-2 rounded-[40px]">
-    <h3 class="text-3xl text-white ml-3 mb-3">{{ props.groupName }}</h3>
-    <div class="text-white grid grid-cols-2 lg:grid-cols-3">
-      <div v-for="entry in props.entries" :key="entry.name" class="flex items-center gap-2">
-        <img :src="entry.icon" alt="icon">
-        <p>{{ entry.name }}</p>
+  <div class="max-w-[800px] mx-auto p-5 my-5">
+    <div class="text-white text-xl flex justify-center">
+      <div>
+        <div v-for="entry in props.entries" :key="entry.name" class="flex items-center gap-2 w-fit mb-3">
+          <img :src="entry.icon" alt="icon" class="w-16">
+          <p>{{ entry.name }}</p>
+        </div>
       </div>
     </div>
   </div>
